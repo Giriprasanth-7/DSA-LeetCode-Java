5a41de505c16e801eaebaeb948745312bfc86ae3
Giriprasanth-7 <giriprasanth484@gmail.com>
2026-09-13 18:51:16 +0530

    Implement minimum moves to clean classroom solution

---
+++ BFS/Minimum_Moves_to_Clean_the_Classroom.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+class Solution {
+    public int minMoves(String[] classroom, int energy) {
+        int m = classroom.length;
+        int n = classroom[0].length();
+        int sr = 0, sc = 0, count = 0;
+
+        int[][] id = new int[m][n];
+        for (int[] row : id) {
+            Arrays.fill(row, -1);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char ch = classroom[i].charAt(j);
+
+                if (ch == 'S') {
+                    sr = i;
+                    sc = j;
+                } else if (ch == 'L') {
+                    id[i][j] = count++;
+                }
+            }
+        }
+
+        int target = (1 << count) - 1;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc, energy, 0, 0});
+
+        boolean[][][][] visited = new boolean[m][n][energy + 1][1 << count];
+        visited[sr][sc][energy][0] = true;
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+            int e = cur[2];
+            int mask = cur[3];
+            int moves = cur[4];
+
+            if (mask == target) {
+                return moves;
+            }
+
+            if (e == 0) {
+                continue;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                    continue;
+                }
+
+                if (classroom[nr].charAt(nc) == 'X') {
+                    continue;
+                }
+
+                int ne = e - 1;
+                int nm = mask;
+                char ch = classroom[nr].charAt(nc);
+
+                if (ch == 'L') {
+                    nm |= 1 << id[nr][nc];
+                }
+
+                if (ch == 'R') {
+                    ne = energy;
+                }
+
+                if (!visited[nr][nc][ne][nm]) {
+                    visited[nr][nc][ne][nm] = true;
+                    q.offer(new int[]{nr, nc, ne, nm, moves + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
